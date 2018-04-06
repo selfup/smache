2cc08a7cc078bee6a70e4ba1038cde4c530b68c5
selfup <boudinot.regis@yahoo.com>
2018-04-06 10:37:07 -0500

    jimmy

--- mitigator/main.go
+++ mitigator/main.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync"
+)
+
+var (
+	mutex = &sync.Mutex{}
+	count = 0
+	ips   = [2]string{"0.0.0.0:1234", "0.0.0.0:1235"}
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	mutex.Lock()
+	uri := fmt.Sprintf("http://%s%s", ips[0], "/api/?key=1")
+
+	if count == 1 {
+		count--
+	} else {
+		count++
+	}
+	mutex.Unlock()
+
+	res, err := http.Get(uri)
+
+	if err != nil {
+		fmt.Fprintf(w, "call to node failed")
+	}
+
+	io.Copy(w, res.Body)
 }
 
 func main() {
