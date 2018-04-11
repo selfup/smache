a5c70e6e7d57a2f6c60b758fcf17fab5d5697a67
selfup <boudinot.regis@yahoo.com>
2018-04-11 00:16:34 -0500

    remove fasthttp from makefile - cleanup bench script

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,31 +4,19 @@ SMACHE_LOG_FILE=.results.smache.log
 PUB_SUB_LOG_FILE=.results.ps.log
 MITIGATOR_LOG_FILE=.results.mitigator.log
 
-function cflag () {
-  if [ "$1" == "-c" ] || [ "$1" == "m" ]
-  then
-    echo "--> entire benchmark output: "
-  else
-    git checkout -- $SMACHE_LOG_FILE
-    git checkout -- $PUB_SUB_LOG_FILE
-    git checkout -- $MITIGATOR_LOG_FILE
-  fi
-}
-
 function run () {
   if [ "$1" == "m" ]
   then
     ab \
       -n 400000 \
-      -c 1000 \
+      -c 400 \
       -k -v 1 \
-      "http://192.168.1.7:8081/" > $MITIGATOR_LOG_FILE \
+      "http://0.0:8081/" > $MITIGATOR_LOG_FILE \
       && echo "" \
       && echo "--> results:
         $(grep seconds $MITIGATOR_LOG_FILE)
         $(grep -w second $MITIGATOR_LOG_FILE)
-      " \
-      && cflag $1
+      "
   fi
 
   if [ "$1" == "" ]
@@ -45,30 +33,34 @@ function run () {
       $(grep seconds $SMACHE_LOG_FILE)
       $(grep -w second $SMACHE_LOG_FILE)
     " \
-    && cflag $1 \
     && ab \
       -n 20000 \
       -c 200 \
       -k -v 1 \
-      "http://0.0.0.0:4001/api/" > $PUB_SUB_LOG_FILE \
+      "http://0.0:4001/api/" > $PUB_SUB_LOG_FILE \
       && echo "" \
       && echo "--> results:
         $(grep seconds $PUB_SUB_LOG_FILE)
         $(grep -w second $PUB_SUB_LOG_FILE)
       " \
-    && cflag $1 \
     && ab \
       -n 40000 \
       -c 400 \
       -k -v 1 \
-      "http://0.0.0.0:8081/" > $MITIGATOR_LOG_FILE \
+      "http://0.0:8081/" > $MITIGATOR_LOG_FILE \
       && echo "" \
       && echo "--> results:
         $(grep seconds $MITIGATOR_LOG_FILE)
         $(grep -w second $MITIGATOR_LOG_FILE)
-      " \
-      && cflag $1
+      "
+  fi
+
+  if [ "$2" == "c" ]
+  then
+    git checkout -- $SMACHE_LOG_FILE
+    git checkout -- $PUB_SUB_LOG_FILE
+    git checkout -- $MITIGATOR_LOG_FILE
   fi
 }
 
-run $1
+run $1 $2
