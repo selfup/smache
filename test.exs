a58c68a523415ce62a073e838b28928b3e3505e5
selfup <boudinot.regis@yahoo.com>
2018-04-12 19:35:44 -0500

    remove test.exs

--- test.exs
+++
@@ -1,8 +0,0 @@
-{:ok, ifs} = :inet.getif()
-
-ips =
-  Enum.map(ifs, fn {ip, _b, _m} ->
-    Enum.join(Tuple.to_list(ip), ".")
-  end)
-
-:os.cmd('echo "-sname #{Enum.at(ips, 0)}" > #{"./rel/vm.args"}')
