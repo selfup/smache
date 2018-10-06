eb3fdc0fb683869ebe599f2e14d5eaeb01125b20
selfup <boudinot.regis@yahoo.com>
2018-10-06 15:35:57 -0500

    change to sync/join pattern and add more comments

--- lib/smache_web/channels/pubsub_channel.ex
+++ lib/smache_web/channels/pubsub_channel.ex
@@ -4,34 +4,37 @@ defmodule SmacheWeb.PubSub do
   alias Smache.Normalizer, as: Normalizer
   alias Smache.Mitigator, as: Mitigator
 
-  # subscribe to all new data updates  
-  def join("room:subscribe" <> _key, _message, socket) do
+  # sync to all new data updates  
+  def join("room:sync" <> _key, _message, socket) do
     {:ok, socket}
   end
 
   # on initial join, ask for current state without updating
-  def join("room:init", _message, socket) do
+  def join("room:join", _message, socket) do
     {:ok, socket}
   end
 
   # pass a scoped name or a scope subscription name
-  def join("room:*" <> _scoped_subscribe, _message, socket) do
+  def join("room:*" <> _scoped, _message, socket) do
     {:ok, socket}
   end
 
-  # subscribe to all new data updates  
-  def handle_in("subscribe", %{"body" => body}, socket) do
-    update(body, "subscribe", socket)
+  # sync to all new data updates  
+  def handle_in("sync", %{"body" => body}, socket) do
+    update(body, "sync", socket)
   end
 
   # on initial join, ask for current state without updating
-  def handle_in("init", %{"body" => body}, socket) do
-    get(body, "init", socket)
+  def handle_in("join", %{"body" => body}, socket) do
+    get(body, "join", socket)
   end
 
   # pass a scoped name or a scope subscription name
+  # if you include the word 'sync' in your room
+  # it will behave like a sync hook
+  # otherwise it will behave like a join hook
   def handle_in(scoped, %{"body" => body}, socket) do
-    case scoped =~ "subscribe" do
+    case scoped =~ "sync" do
       true ->
         update(body, scoped, socket)
 
@@ -53,7 +56,7 @@ defmodule SmacheWeb.PubSub do
     {:noreply, socket}
   end
 
-  # read or update
+  # read if no change or update if there is a change
   defp update(body, room, socket) do
     %{"key" => key, "data" => data} = body
 
