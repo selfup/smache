119319070e97c0e8315eab7263944acf754b96ea
selfup <boudinot.regis@yahoo.com>
2018-10-06 14:52:04 -0500

    update websocket support

--- lib/smache_web/channels/pubsub_channel.ex
+++ lib/smache_web/channels/pubsub_channel.ex
@@ -4,34 +4,62 @@ defmodule SmacheWeb.PubSub do
   alias Smache.Normalizer, as: Normalizer
   alias Smache.Mitigator, as: Mitigator
 
-  def join("room:all" <> _key, _message, socket) do
+  def join("room:subscribe" <> _key, _message, socket) do
     {:ok, socket}
   end
 
-  def join("room:players", _message, socket) do
+  def join("room:init", _message, socket) do
     {:ok, socket}
   end
 
-  def handle_in("sync", %{"body" => body}, socket) do
+  def join("room:*" <> _scoped_subscribe, _message, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("subscribe", %{"body" => body}, socket) do
     %{"key" => key, "data" => data} = body
 
     update =
       Normalizer.normalize(key)
       |> Mitigator.fetch(data)
 
-    broadcast!(socket, "sync", %{key: key, payload: update})
+    broadcast!(socket, "subscribe", %{key: key, payload: update})
 
     {:noreply, socket}
   end
 
-  def handle_in("players", %{"body" => body}, socket) do
+  def handle_in("init", %{"body" => body}, socket) do
     %{"key" => key} = body
 
     {_node, data} =
       Normalizer.normalize(key)
       |> Mitigator.grab_data
 
-    broadcast!(socket, "players", %{key: key, payload: data})
+    broadcast!(socket, "init", %{key: key, payload: data})
+
+    {:noreply, socket}
+  end
+
+  def handle_in(scoped_subscribe, %{"body" => body}, socket) when scoped_subscribe =~ "subscribe" do
+    %{"key" => key, "data" => data} = body
+
+    update =
+      Normalizer.normalize(key)
+      |> Mitigator.fetch(data)
+
+    broadcast!(socket, scoped_room, %{key: key, payload: update})
+
+    {:noreply, socket}
+  end
+
+  def handle_in(scoped_init, %{"body" => body}, socket) do
+    %{"key" => key, "data" => data} = body
+
+    update =
+      Normalizer.normalize(key)
+      |> Mitigator.fetch(data)
+
+    broadcast!(socket, scoped_room, %{key: key, payload: update})
 
     {:noreply, socket}
   end
