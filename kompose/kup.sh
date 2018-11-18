b53954bafef3ee853973de3b8d19bdb5e0c48590
selfup <boudinot.regis@yahoo.com>
2018-11-18 05:23:43 -0600

    minikube and kompose refactor

--- kompose/kup.sh
+++ kompose/kup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-./minikube/docker.send.sh smache_uplink \
-  && ./minikube/docker.send.sh smache_downlink \
-  && kompose up -f kompose/uplink-deployment.yaml -f kompose/downlink-deployment.yaml \
+kompose up -f kompose/uplink-deployment.yaml \
   && kubectl get deployment,svc,pods
--- minikube/README.md
+++ minikube/README.md
@@ -5,9 +5,3 @@ minikube start
 minikube stop
 minikube ssh
 ```
-
-As a workaround to building locally and not pushing to dockerhub
-
-Send local docker image to minikube
-
-Consider GitLab registry??
