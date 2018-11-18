d9f2544c79bb5ef80f9a7265120ad3e12a1ecb77
selfup <boudinot.regis@yahoo.com>
2018-11-18 05:36:50 -0600

    update kompose script

--- kompose/kup.sh
+++ kompose/kup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kompose up -f kompose/uplink-deployment.yaml \
+kompose up -f kompose/uplink-deployment.yaml -f kompose/downlink-deployment.yaml \
   && kubectl get deployment,svc,pods
