7ce0026a85e0c9d3226aa30413791d65da55e7e0
selfup <boudinot.regis@yahoo.com>
2019-03-13 12:34:55 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Smache.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
+      {:phoenix, "~> 1.4.1"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
