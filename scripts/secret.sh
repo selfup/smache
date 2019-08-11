16bdf281c9d932b2eaf134138c5bf2fa194b487b
selfup <boudinot.regis@yahoo.com>
2019-08-11 08:58:30 -0500

    update secret to shell only

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -1,22 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # generates a strong secret and rpc cookie
 
-function genSec32() {
-  openssl rand -base64 32
-}
-
-function genSec64() {
-  openssl rand -base64 64
-}
-
-function genEnv() {
-  echo "
-export SECRET_KEY_BASE=$genSec64
-export COOKIE=$genSec32
-  "
-}
-
 echo 'GENERATING SECRET_KEY_BASE AND COOKIE' \
-  && genEnv \
+  && echo "
+export SECRET_KEY_BASE=$(openssl rand -base64 32)
+export COOKIE=$(openssl rand -base64 32)" > .env \
   && echo 'SECRETS JOB DONE'
